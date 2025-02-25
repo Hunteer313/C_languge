31a76815b2ec0f687e82ec86cd9de12e25c05740
Hunteer <bih.zakaria1@gmail.com>
2025-02-25 15:45:15 +0100

    Scanf

---
+++ Day4/Scanf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y, r;
+
+    printf("enter the value of x: \t");
+    
+    scanf("%d", &x);
+    printf("enter the value of y: \t");
+    
+    scanf("%d", &y);
+
+    r = x+y;
+    printf("The Result of x+y is: %d", r);
+
+        return 0;
+}
