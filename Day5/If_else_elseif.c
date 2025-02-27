94c4b4ccf474e69ec33d2aedd1d806405d910fae
Hunteer <bih.zakaria1@gmail.com>
2025-02-27 20:16:35 +0100

    If_else

---
+++ Day5/If_else_elseif.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // PIN a number brtween 1000 to 9999 
+    int PIN;
+
+    printf("Enter the PIN code ");
+    scanf("%d", &PIN);
+
+    if(PIN < 1000 || PIN > 9999) 
+        {printf("The PIN code must be between 1000 and 9999.\n");}
+    else if(PIN == 1234)
+        {printf("Unlockes!\n");}
+    else
+        {printf("Plese, verify tour PIN code.\n");}
+
+        return 0;
+
+}
