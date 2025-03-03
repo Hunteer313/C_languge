343a6755f304b3eff8af45f822559fc3a83f62a1
Hunteer <bih.zakaria1@gmail.com>
2025-03-03 01:06:07 +0100

    While

---
+++ Day6/While.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int PIN, i=1;
+    printf("Enter your PIN Code:\t");
+    scanf("%d", &PIN);
+    
+    while (PIN != 1234){
+
+        i++;
+        if(i>3)
+        {
+            printf("Your Accunt is Locked!\n");
+            break;  
+        }
+        printf("PIN incorrect!\n");
+
+        printf("Enter your PIN Code:\t");
+        
+        scanf("%d", &PIN);
+    }   
+        return 0;
+
+}
