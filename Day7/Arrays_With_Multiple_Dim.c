9f48d578b73aaee640a0880985f337ddf2deb855
Hunteer <bih.zakaria1@gmail.com>
2025-03-08 04:06:11 +0100

    Arrays

---
+++ Day7/Arrays_With_Multiple_Dim.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char Week[7][10]={
+        {"Monday"},
+        {"tuesday"},
+        {"Wednesday"},
+        {"Thursday"},
+        {"friday"},
+        {"Saturday"},
+        {"Sunday"}
+    };
+    printf("%s\n", Week[2]);
+}
